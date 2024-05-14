e26a0d7c4ed82a6c4db5e971acc355b84aea97a0
Rayan Albertassi <rayan.albertassi@hotmail.com>
2024-05-13 21:08:45 -0300

    aula 12 metade

---
+++ CURSOEMVIDEO/aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 12;
+if (idade > 18) {
+    console.log("Vota.");
+} else {
+    if (idade > 16)
+        console.log("Pode votar.");
+    else{
+        console.log("Não vota.");
+    }
+}
