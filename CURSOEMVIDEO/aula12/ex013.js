ef02e90981f5067f641b204f42ff6a7199f96ac3
Rayan Albertassi <rayan.albertassi@hotmail.com>
2024-05-13 21:38:36 -0300

    aula12

---
+++ CURSOEMVIDEO/aula12/ex013.js
@@ -0,0 +1,36 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+switch(diaSem){
+    case 0:
+        console.log("Hoje é domingo.")
+        break
+    
+    case 1:
+        console.log("Hoje é Segunda-feira.")
+        break
+    
+    case 2:
+        console.log("Hoje é Terça-feira.")
+        break
+    
+    case 3:
+        console.log("Hoje é Quarta-feira.")
+        break
+    
+    case 4:
+        console.log("Hoje é Quinta-feira.")
+        break
+    
+    case 5:
+        console.log("Hoje é Sexta-feira.")
+        break
+    
+    case 6:
+        console.log("Hoje é Sábado-feira.") 
+        break
+
+    default:
+        console.log("Erro: dia inválido.")
+    break
+}
