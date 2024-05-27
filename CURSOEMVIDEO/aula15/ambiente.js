9c3a317d788648fa55d21b6032d42779780ff5a7
Rayan Albertassi <rayan.albertassi@hotmail.com>
2024-05-26 21:46:15 -0300

    aula 15 terminada

--- CURSOEMVIDEO/aula15/ambiente.js
+++ CURSOEMVIDEO/aula15/ambiente.js
@@ -5,3 +5,9 @@ num.sort();
 
 console.log(`Nosso vetor é o ${num} `);
 console.log(num.length);
+
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos + 1}`)
+
+
+2, 3, 5, 6, 7, 8, 9
---
+++ CURSOEMVIDEO/aula15/vetor.js
@@ -0,0 +1,10 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+for(let c = 0; c < valores.length; c++){
+    console.log(`A posição ${c + 1} tem o valor ${valores[c]}`);
+};
+
+
+/* for(let c in valores){
+    console.log(`A posição ${c} tem o valor ${valores[c]}`);
+} */
